e3d833515df80f5c635841e3ab1f0b335bf4961e
ihando <ihando728@gmail.com>
2024-05-26 02:22:16 -0400

    autocomplete for form off all inputs

--- src/addsubmitclose.js
+++ src/addsubmitclose.js
@@ -13,26 +13,28 @@ function addTaskForm() {
         form.classList.add("taskform");
         form.innerHTML = `
         <label for="name"></label>
-        <input type="text" id="name" name="name" placeholder="Task name (required)" required><br>
+        <input type="text" id="name" name="name" placeholder="Task name (required)" required autocomplete="off"><br>
 
         <label for="date"></label>
-        <input type="date" id="date" name="date" required><br>
+        <input type="date" id="date" name="date" required autocomplete="off"><br>
 
-        <select name="prio" id="prio">
+        <label for="prio"></label>
+        <select name="prio" id="prio" autocomplete="off">
             <option value="none" selected>No priority</option>
             <option value="low">Low priority</option>
             <option value="mid">Medium priority</option>
             <option value="high">High priority</option>
         </select><br>
 
-        <select name="tasks" id="tasks">
+        <label for="tasks"></label>
+        <select name="tasks" id="tasks" autocomplete="off">
             <option value="all">Tasks</option>
             <option value="project1">Project 1</option>
             <option value="project2">Project 2</option>
         </select>
 
         <label for="descrip"></label>
-        <input type="text" id="descrip" name="descrip" placeholder="Notes (optional)">
+        <input type="text" id="descrip" name="descrip" placeholder="Notes (optional)" autocomplete="off">
 
         <button type="button" class="button close">Close</button>
         <button type="submit" class="button submitButton">Submit</button>
